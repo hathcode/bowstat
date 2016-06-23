481ad1517e141443c161f1dd73aa385eff1800b6
Kevin Hathorn <hathornk@hotmail.com>
2016-06-23 18:09:04 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bowstat::Application.config.secret_key_base = 'a72626fa3d74ad924dd22e9a40efa3e4fd7263a3c7e799911a40a01520a07b8637814bb71df84c5efc48673fb82d54350c42edda77a13d456573351fdbe8677b'
