cccc1df9c4ad79f4ef61447553c0fe29b0e9569d
Kevin Hathorn <hathornk@hotmail.com>
2016-06-23 18:45:58 +0000

    Set up bows controller, added TBS3

---
+++ app/controllers/bows_controller.rb
@@ -0,0 +1,5 @@
+class BowsController < ApplicationController
+    def index
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bowstat::Application.routes.draw do
+
+  root 'bows#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
