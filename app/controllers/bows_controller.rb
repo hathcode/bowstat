a6924b9ba46abcab5ad7aa496e3f16c08a440a3a
Kevin Hathorn <hathornk@hotmail.com>
2016-06-24 05:46:10 +0000

    Configured form and index links, show data on index page

--- app/controllers/bows_controller.rb
+++ app/controllers/bows_controller.rb
@@ -1,8 +1,21 @@
 class BowsController < ApplicationController
     def index
+        @stats = Stats.all
     end
 
     def new
         @stats = Stats.new
     end
+
+    def create
+        Stats.create(stats_params)
+        redirect_to root_path
+    end
+
+    private
+
+    def stats_params
+        params.require(:stats).permit(:ttt_length, :ntn_length, :type, :handle, :draw_length_inches, :draw_weight_pounds, :arrow_speed_fps, :wood_type)
+    end
+
 end
